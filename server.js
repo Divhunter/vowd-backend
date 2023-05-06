8fa18548907f00d866940fd4abfec270e9109ff1
Aaron <aarondivi@gmail.com>
2023-05-06 10:42:24 +0200

    init

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const normalizePort = val => {
   return false;
 };
 
-const port = normalizePort(process.env.PORT || '4200');
+const port = normalizePort(process.env.PORT || '1000');
 app.set('port', port);
 
 const errorHandler = error => {
