605ae09fc51d49e9a1d724014b42a5c718d198cc
Aaron <aarondivi@gmail.com>
2023-05-07 16:28:20 +0200

    init

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use(express.urlencoded({extended: false}));
 
 // Définition des entêtes CORS
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', '*', 'https://vowd-project-app-api.onrender.com');
     res.setHeader(
       "Access-Control-Allow-Headers",
       "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization, Content-Type, Access-Control-Allow-Headers"
