67fcc35757c2919cbcfb319358917c7ca8a0211e
Aaron <aarondivi@gmail.com>
2023-05-22 14:44:38 +0200

    init

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.use(express.urlencoded({extended: false}));
 app.use((req, res, next) => {
     res.setHeader(
     'Access-Control-Allow-Origin',
-    '*'
+    'https://vowd-project.onrender.com'
     );
     res.setHeader(
       "Access-Control-Allow-Headers",
