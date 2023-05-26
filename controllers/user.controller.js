ca5468c681572c1db9bea9facb1f46e6621169c5
Aaron <aarondivi@gmail.com>
2023-05-26 13:31:56 +0200

    init

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -103,43 +103,42 @@ module.exports.login = (req, res, next) => {
 // Relatif à l'envoi du mail d'authentification'
 
 module.exports.sendMail = (req, res, next) => {
-    const userName = req.body.userName
-    const email = req.body.email
-    const verifUser = { userName: userName, email: email }
-    UserModel.findOne(verifUser) 
-    if (!verifUser) {
-        return res.json({ userSendError: 'Erreur d\'authentification !' }).status(401);
-    } 
-    else {
-        const transporter = nodeMailer.createTransport({
-            host: 'smtp-mail.outlook.com',
-            secureConnection: false,
-            port: 587,
-            tls: {
-                ciphers:"SSLv3"
-            },
-            auth: {
-                user: process.env.EMAIL,
-                pass: process.env.PASSWORD
-            }
-        })
-        
-        const mailOptions = {
-            from: process.env.EMAIL,
-            to: email,
-            subject: 'Réinitialisation de mot de passe',
-            html: `<p>Bonjour ${userName}, voici le lien pour réinitialiser votre mot de passe: ${process.env.CLIENT_URL}/password </p>`
+    UserModel.findOne({ userName: req.body.userName, email: req.body.email })
+    .then(user => {
+        if (!user) {
+            return res.json({ userSendError: 'Ce compte n\'existe pas !' }).status(401);
         }
-    
-        transporter.sendMail(mailOptions, error => {
-            if (error) {
-                res.json({ error: 'Une erreur inattendue est survenue, veuillez réesayer ulterieurement !' }).status(400)
-            } 
-            else {
-                res.json({ messageSend: userName +', nous traitons votre demande !' }).status(201)
-            };
-        })
-    };
+        else {
+            const transporter = nodeMailer.createTransport({
+                host: 'smtp-mail.outlook.com',
+                secureConnection: false,
+                port: 587,
+                tls: {
+                    ciphers:"SSLv3"
+                },
+                auth: {
+                    user: process.env.EMAIL,
+                    pass: process.env.PASSWORD
+                }
+            })
+        
+            const mailOptions = {
+                from: process.env.EMAIL,
+                to: email,
+                subject: 'Réinitialisation de mot de passe',
+                html: `<p>Bonjour ${userName}, voici le lien pour réinitialiser votre mot de passe: ${process.env.CLIENT_URL}/password </p>`
+            }
+        
+            transporter.sendMail(mailOptions, error => {
+                if (error) {
+                    res.json({ error: 'Une erreur inattendue est survenue, veuillez réesayer ulterieurement !' }).status(400)
+                } 
+                else {
+                    res.json({ messageSend: userName +', nous traitons votre demande !' }).status(201)
+                };
+            })
+        };
+    })
 }
 
 
