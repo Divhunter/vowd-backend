9989f7e565c7c68047f50a34a9beae9e8242ecdb
Aaron <aarondivi@gmail.com>
2023-05-27 01:07:15 +0200

    init

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -123,16 +123,16 @@ module.exports.sendMail = (req, res, next) => {
         html: `<p>Bonjour ${userName}, voici le lien pour réinitialiser votre mot de passe <a href = "http://localhost:3000/monSite/update_password" >réinitialisation</a></p>`
     }
 
+    UserModel.findOne({ userName: req.body.userName, email: req.body.email })
+    .then(user => {
+        if (!user) {
+            return res.json({ userLogError: 'Ce compte d\'utilisateur n\'existe pas !' }).status(401);
+        }
     transporter.sendMail(mailOptions, () => {
-        UserModel.findOne({ userName: req.body.userName, email: req.body.email })
-        .then(user => {
-            if (!user) {
-                return res.json({ userLogError: 'Ce compte d\'utilisateur n\'existe pas !' }).status(401);
-            }
             return res.json({ messageSend: userName +', nous traitons votre demande !' }).status(201)
         })
-        .catch(error => res.json({ error: 'Une erreur inattendue est survenue, veuillez réesayer ulterieurement !' }).status(500));
     })
+    .catch(error => res.json({ error: 'Une erreur inattendue est survenue, veuillez réesayer ulterieurement !' }).status(500));
 }
 
 
