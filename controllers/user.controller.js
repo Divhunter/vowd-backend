678621b15a09adcf99d880f95bb5be6b5c81a913
Aaron <aarondivi@gmail.com>
2023-05-27 08:45:55 +0200

    init

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -103,32 +103,32 @@ module.exports.login = (req, res, next) => {
 // Relatif à l'envoi du mail d'authentification'
 
 module.exports.sendMail = (req, res, next) => {
-    const transporter = nodeMailer.createTransport({
-        host: 'smtp-mail.outlook.com',
-        secureConnection: false,
-        port: 587,
-        tls: {
-            ciphers:"SSLv3"
-        },
-        auth: {
-            user: process.env.EMAIL,
-            pass: process.env.PASSWORD
-        }
-    })
-
-    const mailOptions = {
-        from: process.env.EMAIL,
-        to: email,
-        subject: 'Réinitialisation de mot de passe',
-        html: `<p>Bonjour ${userName}, voici le lien pour réinitialiser votre mot de passe <a href = "http://localhost:3000/monSite/update_password" >réinitialisation</a></p>`
-    }
-
     UserModel.findOne({ userName: req.body.userName, email: req.body.email })
     .then(user => {
         if (!user) {
             return res.json({ userLogError: 'Ce compte d\'utilisateur n\'existe pas !' }).status(401);
         }
-    transporter.sendMail(mailOptions, () => {
+        const transporter = nodeMailer.createTransport({
+            host: 'smtp-mail.outlook.com',
+            secureConnection: false,
+            port: 587,
+            tls: {
+                ciphers:"SSLv3"
+            },
+            auth: {
+                user: process.env.EMAIL,
+                pass: process.env.PASSWORD
+            }
+        })
+    
+        const mailOptions = {
+            from: process.env.EMAIL,
+            to: email,
+            subject: 'Réinitialisation de mot de passe',
+            html: `<p>Bonjour ${userName}, voici le lien pour réinitialiser votre mot de passe <a href = "http://localhost:3000/monSite/update_password" >réinitialisation</a></p>`
+        }
+        
+        transporter.sendMail(mailOptions, () => {
             return res.json({ messageSend: userName +', nous traitons votre demande !' }).status(201)
         })
     })
