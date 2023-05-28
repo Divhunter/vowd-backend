dce1a68f01f63819b0f6ad34bbf8230111ebf30e
Aaron <aarondivi@gmail.com>
2023-05-28 16:38:21 +0200

    init

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -42,7 +42,7 @@ passwordSchema
 module.exports.register = (req, res, next) => {
     UserModel.findOne({ userName: req.body.userName, email: req.body.email })
     .then(user => {
-        if (user) {
+        if (user === user.userName || user === user.email) {
             return res.json({ userRegError: 'Pseudo et/ou email déjà utilisés !' }).status(400);
         }
         else if (!regexUserName.test(req.body.userName)) {
